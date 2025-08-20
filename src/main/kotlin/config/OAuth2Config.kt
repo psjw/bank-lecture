c6030fa73572bdbf181223f3e53ed8f5305118b7
psjw <dlrudtn108@gmail.com>
2025-08-20 16:19:23 +0900

    OAuth Config 구현

---
+++ src/main/kotlin/config/OAuth2Config.kt
@@ -0,0 +1,30 @@
+package com.psjw.basic.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+/*
+    oauth2:
+        providers:
+            google:
+                client-id: sample
+                client-secret: <>
+                redirect-uri: <>
+            kakao:
+                client-id: sample
+                client-secret: <>
+                redirect-uri: <>
+ */
+
+@Configuration
+@ConfigurationProperties(prefix = "oauth2")
+class OAuth2Config {
+    val providers: MutableMap<String, OAuth2ProviderValues> = mutableMapOf()
+}
+
+
+data class OAuth2ProviderValues(
+    val clientId: String,
+    val clientSecret: String,
+    val redirectUri: String
+)
---
+++ src/main/kotlin/interfaces/OAuthServiceInterface.kt
@@ -0,0 +1,18 @@
+package com.psjw.basic.interfaces
+
+interface OAuthServiceInterface {
+    val providerName: String
+    fun getToken(code: String): OAuth2TokenResponse
+    fun getUserInfo(accessToken: String): OAuth2UserResponse
+}
+
+
+interface OAuth2TokenResponse {
+    val accessToken: String
+}
+
+interface OAuth2UserResponse {
+    val id: String
+    val email: String? //? null or String
+    val name: String?
+}
